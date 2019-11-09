c2890c3c304dda2158d62686009b601966f62aae
Konrad Kalita <konradkalita@student.agh.edu.pl>
2019-11-09 14:52:48 +0100

    Filter_out_the_geese

---
+++ Filter_out_the_geese/solver.js
@@ -0,0 +1,7 @@
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter(bird => {
+    return geese.includes(bird) == false ? bird : ""
+  });
+  // return an array containing all of the strings in the input array except those that match strings in geese
+};
